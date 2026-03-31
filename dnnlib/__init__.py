4d4ba904fbd04749d43473307315973f545818d4
Leos <158104435+LeosCtrt@users.noreply.github.com>
2026-03-31 21:11:32 +0200

    Delete dnnlib directory

--- dnnlib/__init__.py
+++
@@ -1,9 +0,0 @@
-# This directory should contain files from the CB-AE repo.
-# Run: python setup.py --copy-vendor /path/to/posthoc-generative-cbm
-# Or manually copy:
-#   cbae_repo/dnnlib/__init__.py  ->  dnnlib/__init__.py
-#   cbae_repo/dnnlib/util.py      ->  dnnlib/util.py
-raise ImportError(
-    "dnnlib/ is empty. Copy it from the CB-AE repo.\n"
-    "Run: python setup.py --copy-vendor /path/to/posthoc-generative-cbm"
-)
