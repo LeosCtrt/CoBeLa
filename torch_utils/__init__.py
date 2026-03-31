74fc8cb87b11974639ae8557989596e9df6dca8e
Leos <158104435+LeosCtrt@users.noreply.github.com>
2026-03-31 21:11:24 +0200

    Delete torch_utils directory

--- torch_utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# This directory should contain files from the CB-AE repo.
-# Run: python setup.py --copy-vendor /path/to/posthoc-generative-cbm
-raise ImportError(
-    "torch_utils/ is empty. Copy it from the CB-AE repo.\n"
-    "Run: python setup.py --copy-vendor /path/to/posthoc-generative-cbm"
-)
